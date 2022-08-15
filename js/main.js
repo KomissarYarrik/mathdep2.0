444ec5a068c7ae3f6bd6a8cd5f94d09bdac42e72
RuslanAntonov <102322592+RuslanAntonov@users.noreply.github.com>
2022-08-16 01:11:39 +0300

    a

---
+++ js/main.js
@@ -0,0 +1,66 @@
+// --- menu scroll ---
+
+const header = document.querySelector('.header');
+const first = document.querySelector('.welcome');
+const headerHeight = header.offsetHeight;
+const firstHeight = first.offsetHeight;
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', () => {
+	let scrollDistance = window.scrollY;
+    if (scrollDistance > lastScrollTop) {
+		header.classList.remove('header--fixed');
+		first.style.marginTop = null;
+	} else {
+		header.classList.add('header--fixed');
+		
+	}
+
+	if (scrollDistance === 0) {
+		header.classList.remove('header--fixed');
+		first.style.marginTop = null;
+	}
+
+	lastScrollTop = scrollDistance;
+});
+
+
+// --- mobile menu ---
+
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger, .header__menu').toggleClass('active')
+        $('body').toggleClass('lock');
+        $('.header').toggleClass('header--mob-menu');
+    });
+
+    // If a link has a dropdown, add sub menu toggle.
+    $('.header__link:not(:only-child)').click(function (e) {
+        $(this).siblings('.header__dropdown').slideToggle("slow");
+
+        // Close dropdown when select another dropdown
+        $('.header__dropdown').not($(this).siblings()).slideUp("slow");
+        e.stopPropagation();
+    });
+
+    // Click outside the dropdown will remove the dropdown class
+    $('html').click(function () {
+        $('.header__dropdown').slideUp("slow");
+    });
+});
+
+
+// --- swiper ---
+
+var swiper = new Swiper(".welcome-swiper", {
+    direction: "vertical",
+    loop: true,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
